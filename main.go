1259180b6ac69a77303e151b4c6f272d1601fb84
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-29 18:41:06 -0700

    Finalized program

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/DrItanium/unicornhat"
-	"math"
+	"log"
+	"math/rand"
+	"runtime"
+	"time"
 )
 
 type Memory []byte
@@ -16,6 +19,8 @@ const (
 	MemorySize = 128 * Meg
 	CpuStart   = 64 * Meg
 	OneMeg     = 1 * Meg
+	MemSize    = 16 * Meg
+	µcoreSize  = MemSize / 64
 )
 
 type µcore struct {
@@ -25,10 +30,19 @@ type µcore struct {
 }
 
 func (this *µcore) Execute() {
-	for i := 0; i < this.memory; i += 4 {
-		unicornhat.SetPixelColor(this.index, this.memory[0], this.memory[1], this.memory[2])
+	log.Printf("µcore %d: Randomizing memory", this.index)
+	for i := 0; i < len(this.memory); i++ {
+		this.memory[i] = byte(rand.Uint32())
 	}
+	log.Printf("µcore %d: Done randomizing memory", this.index)
+	log.Printf("µcore %d: Walking through memory", this.index)
+	for i := 0; i < len(this.memory); i += 4 {
+		unicornhat.SetPixelColor(this.index, this.memory[0], this.memory[1], this.memory[2])
+		MicrosecondDelay(time.Duration(this.memory[3]))
+	}
+	log.Printf("µcore %d: done walking", this.index)
 	this.done <- 0
+	close(this.done)
 }
 
 func New(index uint, memory Memory) *µcore {
@@ -42,38 +56,43 @@ func New(index uint, memory Memory) *µcore {
 func main() {
 	var cpus []*µcore
 	var memory Memory
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	defer unicornhat.Terminate(0)
 	unicornhat.SetBrightness(unicornhat.DefaultBrightness())
 	unicornhat.Init(NumCpus)
 	unicornhat.ClearLEDBuffer()
 	cpus = make([]*µcore, NumCpus)
-	memory = make(Memory, 128*(1024*1024))
-	upperHalf := memory[CpuStart:]
+	memory = make(Memory, MemSize)
+	upperHalf := memory
 	for i := 0; i < 64; i++ {
-		targetMem := upperHalf[:OneMeg]
+		targetMem := upperHalf[:µcoreSize]
 		cpus[i] = New(uint(i), targetMem)
 		go cpus[i].Execute()
-		upperHalf = upperHalf[OneMeg:]
+		upperHalf = upperHalf[µcoreSize:]
 	}
 
-	for i := 0; i < len(memory); i++ {
-		memory[i] = byte(math.Uint32())
-	}
 	count := 0
 	for {
-		if count == 64 {
+		if count >= 64 {
 			break
 		} else {
 			for i := 0; i < 64; i++ {
 				c := cpus[i].done
 				select {
 				case <-c:
+					fmt.Println("done")
 					count++
 				default:
+					unicornhat.Show()
+					MicrosecondDelay(10)
 				}
 			}
 			unicornhat.Show()
-			microsecond_delay(10)
+			MicrosecondDelay(10)
 		}
 	}
+}
+
+func MicrosecondDelay(usec time.Duration) {
+	time.Sleep(usec * time.Microsecond)
 }
