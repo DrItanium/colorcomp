ae5d6f80cb0f19815edac7e53a23f4e83371b2b9
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-30 19:08:42 -0700

    Increased memsize to 64-mb

--- main.go
+++ main.go
@@ -13,14 +13,11 @@ type Memory []byte
 type Word uint32
 
 const (
-	NumCpus    = 64
-	Kilo       = 1024
-	Meg        = Kilo * Kilo
-	MemorySize = 128 * Meg
-	CpuStart   = 64 * Meg
-	OneMeg     = 1 * Meg
-	MemSize    = 8 * Meg
-	µcoreSize  = MemSize / 64
+	NumCpus   = 64
+	Kilo      = 1024
+	Meg       = Kilo * Kilo
+	MemSize   = 64 * Meg
+	µcoreSize = MemSize / 64
 )
 
 type µcore struct {
@@ -34,7 +31,7 @@ func (this *µcore) Execute() {
 	log.Printf("µcore %d: Walking through memory", this.index)
 	for i := 0; i < len(this.memory); i += 4 {
 		this.result <- unicornhat.NewPixel(this.memory[i+0], this.memory[i+1], this.memory[i+2])
-		MillisecondDelay(this.memory[i+3] % 17)
+		MillisecondDelay(time.Duration(this.memory[i+3]))
 	}
 	log.Printf("µcore %d: done walking", this.index)
 	this.done <- 0
@@ -55,7 +52,7 @@ func main() {
 	var memory Memory
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	defer unicornhat.Shutdown()
-	unicornhat.SetBrightness(128)
+	unicornhat.SetBrightness(unicornhat.DefaultBrightness / 2)
 	if err := unicornhat.Initialize(); err != nil {
 		fmt.Println(err)
 		return
@@ -66,7 +63,7 @@ func main() {
 	memory = make(Memory, MemSize)
 	log.Print("Randomizing memory")
 	for i := 0; i < len(memory); i++ {
-		memory[i] = byte(rand.Uint32()) % 64
+		memory[i] = byte(rand.Uint32())
 	}
 	log.Print("Done randomizing memory")
 	upperHalf := memory
@@ -91,16 +88,15 @@ func main() {
 			case value := <-c.result:
 				unicornhat.SetPixelColor(c.index, value.R, value.G, value.B)
 			case <-c.done:
-				fmt.Println("done")
 				count++
 				cpus[i] = nil
 			default:
 			}
 		}
 		unicornhat.Show()
-		MillisecondDelay(33)
+		MillisecondDelay(17)
 	}
 }
 func MillisecondDelay(msec time.Duration) {
-	time.Sleep(usec * time.Millisecond)
+	time.Sleep(msec * time.Millisecond)
 }
